2c6a15d0fa70ef023c8511ef645df4bd75b07a13
aantle11 <alicia.antle@keyin.com>
2025-07-10 19:06:55 -0230

    Student starting point & Person set and get added

--- 1/Person.java
+++ 1/Person.java
@@ -9,6 +9,14 @@ public class Person {
         myGender = gender;
     }
     
+    public String getName() { return myName;}
+    public int getAge() { return myAge;}
+    public String getGender() { return myGender;}
+
+    public void setName(String name) { myName = name;}
+    public void setAge(int age) { myAge = age;}
+    public void setGender(String gender) { myGender = gender;}
+    
     public String toString() {
         return myName + ", age: " + myAge + ", gender: " + myGender;
     }
--- 1/Student.java
+++ 1/Student.java
@@ -1,3 +1,11 @@
-public class Student {
-    
+class Student extends Person {
+    protected String myIdNum;
+    protected double myGPA;
+
+    public Student(String name, int age, String gender, String IdNum, double gpa) {
+        super(name, age, gender);
+
+        myIdNum = IdNum;
+        myGPA = gpa;
+    }
 }
