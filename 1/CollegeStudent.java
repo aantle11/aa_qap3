455965207465e1a951d4b3efa6a6febe6fa13e34
aantle11 <alicia.antle@keyin.com>
2025-07-10 21:18:03 -0230

    Teacher & CollegeStudent classes complete ( had weird error with teacher being underlined in line 1 but relaunching vs seemed to fix after )

---
+++ 1/CollegeStudent.java
@@ -0,0 +1,20 @@
+public class CollegeStudent extends Student {
+    private String myMajor;
+    private int myYear;
+
+    public CollegeStudent(String name, int age, String gender, String idNum, double gpa, int year, String major) {
+        super(name, age, gender, idNum, gpa);
+        myYear = year;
+        myMajor = major;
+    }
+
+    public int getYear() { return myYear; }
+    public String getMajor() { return myMajor; }
+
+    public void setYear(int year) { myYear = year; }
+    public void setMajor(String major) { myMajor = major; }
+
+    public String toString() {
+        return super.toString() + ", Year: " + myYear + ", Major: " + myMajor;
+    }
+}
--- 1/Teacher.java
+++ 1/Teacher.java
@@ -1,3 +1,20 @@
-public class Teacher {
-    
+public class Teacher extends Person{
+    private String mySubject;
+    private double mySalary;
+
+    public Teacher(String name, int age, String gender, String subject, double salary) {
+        super(name, age, gender);
+        mySubject = subject;
+        mySalary = salary;
+    }
+
+    public String getSubject() { return mySubject; }
+    public double getSalary() { return mySalary; }
+
+    public void setSubject(String subject) { mySubject = subject; }
+    public void setSalary(double salary) { mySalary = salary; }
+
+    public String toString() {
+        return super.toString() + ", Subject: " + mySubject + ", Salary: $" + mySalary;
+    }
 }
