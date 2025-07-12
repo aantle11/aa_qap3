cafc239956b3f91a31ffd90cd9a0a67b66d99f76
aantle11 <alicia.antle@keyin.com>
2025-07-11 23:35:52 -0230

    Ellipse & Circle class complete

--- 3/Circle.java
+++ 3/Circle.java
@@ -1,3 +1,5 @@
-public class Circle {
-    
+public class Circle extends Ellipse {
+    public Circle(String name, double radius) {
+        super(name, radius, radius);
+    }
 }
--- 3/Ellipse.java
+++ 3/Ellipse.java
@@ -1,3 +1,25 @@
-public class Ellipse {
-    
+public class Ellipse extends Shape {
+    private double a, b;
+
+    public Ellipse(String name, double axis1, double axis2) {
+        super(name);
+        if (axis1 >= axis2) {
+            this.a = axis1;
+            this.b = axis2;
+        } 
+        else {
+            this.a = axis2;
+            this.b = axis1;
+        }
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * a * b;
+    }
+
+    @Override
+    public double perimeter() {
+        return Math.PI * (2 * (a * a + b * b) - Math.pow(a - b, 2)) / 2;
+    }
 }
