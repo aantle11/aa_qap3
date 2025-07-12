f624db04abda9424e07938b53a7571aa56e1eb17
aantle11 <alicia.antle@keyin.com>
2025-07-12 01:10:52 -0230

    shape class complete

--- 4/Shape.java
+++ 4/Shape.java
@@ -1,3 +1,15 @@
-public class Shape {
-    
+public abstract class Shape implements Scalable {
+    protected String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+      return name + " - Area: " + String.format("%.2f", area()) + ", Perimeter: " + String.format("%.2f", perimeter());  
+    }
 }
