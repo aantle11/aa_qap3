79f284ce0b02d89e5bed1f58b563804fda2d8171
aantle11 <alicia.antle@keyin.com>
2025-07-12 00:14:20 -0230

    demo class

--- 3/Demo.java
+++ 3/Demo.java
@@ -1,3 +1,15 @@
 public class Demo {
-    
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[5];
+
+        shapes[0] = new Circle("Circle A", 5.0);
+        shapes[1] = new Ellipse("Ellipse B", 6.0, 4.0);
+        shapes[2] = new Triangle("Triangle C", 3.0, 4.0, 5.0);
+        shapes[3] = new EquilateralTriangle("Equilateral D", 6.0);
+        shapes[4] = new Triangle("Triangle E", 10.0, 8.0, 6.0);
+
+        for (Shape s : shapes) {
+            System.out.println(s);
+        }
+    }
 }
