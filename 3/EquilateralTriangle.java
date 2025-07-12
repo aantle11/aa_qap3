2f1c7a1ecd74218bcc1a2eca5b89e4f9ffb68a1e
aantle11 <alicia.antle@keyin.com>
2025-07-12 00:08:46 -0230

    Triangle & EquilateralTriangle classes complete

--- 3/EquilateralTriangle.java
+++ 3/EquilateralTriangle.java
@@ -1,3 +1,5 @@
-public class EquilateralTriangle {
-    
+public class EquilateralTriangle extends Triangle {
+    public EquilateralTriangle(String name, double side) {
+        super(name, side, side, side);
+    }
 }
--- 3/Triangle.java
+++ 3/Triangle.java
@@ -1,3 +1,26 @@
-public class Triangle {
+public class Triangle extends Shape {
+    private double side1, side2, side3;
+
+    public Triangle(String name, double side1, double side2, double side3) {
+        super(name);
+        if (side1 + side2 <= side3 || side3 + side1 <= side2) {
+            System.out.println("Error");
+            System.exit(1);
+        }
+        
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
     
+    @Override
+    public double perimeter() {
+        return side1 + side2 + side3;
+    }
+
+    @Override
+    private double area() {
+        double s = perimeter() / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
 }
