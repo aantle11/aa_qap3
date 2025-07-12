b77ecb544893f88f6d05db1453eafe95947bd554
aantle11 <alicia.antle@keyin.com>
2025-07-12 02:24:43 -0230

    triangle & equilateraltriangle classes complete

--- 4/EquilateralTriangle.java
+++ 4/EquilateralTriangle.java
@@ -1,3 +1,12 @@
-public class EquilateralTriangle {
-    
+public class EquilateralTriangle extends Triangle {
+    public EquilateralTriangle(String name, double side) {
+        suepr(name, side, side, side);
+    }
+
+    @Override
+    public void scale(double factor) {
+        side1 *= factor;
+        side2 = side1;
+        side3 = side1;
+    }
 }
