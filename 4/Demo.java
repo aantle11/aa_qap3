0a79343730bd9807927a76962ab939cce27674f3
aantle11 <alicia.antle@keyin.com>
2025-07-12 12:15:29 -0230

    demo class complete

--- 4/Demo.java
+++ 4/Demo.java
@@ -1,3 +1,27 @@
 public class Demo {
-    
+    public static void scaleAll(Scalable[] items, double factor) {
+        System.out.println("Before scaling:");
+        for (Scalable s : items) {
+            System.out.println(s);
+        }
+
+        for (Scalable s : items) {
+            s.scale(factor);
+        }
+
+        System.out.println("After scaling:" + factor + ":");
+        for (Scalable s : items) {
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scalable[] shapes = new Scalable[4];
+        shapes[0] = new Circle("Circle A", 5.0);
+        shapes[1] = new Ellipse("Ellipse B", 6.0, 4.0);
+        shapes[2] = new Triangle("Triangle C", 3.0, 4.0, 5.0);
+        shapes[3] = new EquilateralTriangle("Equilateral D", 6.0);
+
+        scaleAll(shapes, 2.0);
+    }
 }
